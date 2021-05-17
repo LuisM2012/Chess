2a2a7b98b3ff36449431c839e6fed1a228995e04
Luis Morales <81542969+LuisM2012@users.noreply.github.com>
2021-05-17 16:34:53 -0700

    improved usability

--- pieces.py
+++ pieces.py
@@ -6,47 +6,34 @@ class Piece():
         self.set_info(kwargs)
         self.player = player
 
-    def set_position(self, position: (int, int), moved: bool = True):
+    def set_position(self, position: tuple([(int, int)]), moved: bool = True):
         self._position = position
-        #Used to keep track of moves made
-        if moved: 
-            self.moved()
-
-    def get_position(self):
-        return self._position
+        
+        if moved:       #Used to keep track of moves made
+            self.moves_made +=1
     
     def set_image(self, image):
         self.image = image
 
-    def set_info(self, info):
+    def set_info(self, info: dict):
         if "name" in info:
             self.name = info["name"]
         else:
             self.name = None
-    
-    def moved(self):
-        self.moves_made +=1
 
-    def check_mate(self, board):
-        king_moves = []
-        players_move =[]
-        for j in board:
-            for i in j:
-                if i!=0 and i.name=="K" and i.player==self.player:
-                    king_moves.append(i.get_position())
-                    king_moves += i.get_moves(board)
-                if i!=0 and i.player !=self.player:
-                    players_move += i.get_moves(board)
-        print(f"other:{players_move}")
-        print(f"king:{king_moves}")
-        return
-
-    def invert_moves(self, moves):
-        r_move = []
-        for item in moves:
-            r_move.append((7-item[0], 7-item[1]))
-
-        return r_move
+    # def checkmate_prevent(self, board): #still in production
+    #     """Check if current player is checkmated."""
+    #     players_move = []
+    #     king_moves = []
+    #     for j in board:
+    #         for i in j:
+    #             if i!=0 and i.name=="K" and i.player==self.current:
+    #                 king_moves += i.get_moves(board, self.screen!=self.current) + [i._position]
+    #             if i!=0 and i.player!=self.current:
+    #                 players_move += i.get_moves(board, self.screen==self.current)
+    #     print(f"other:{players_move}")
+    #     print(f"king:{king_moves}")
+    #     # return
 
     ##Extras
     def get_straight(self, board):
@@ -100,7 +87,7 @@ class Piece():
         return moves
 
     def __str__(self):
-        return self.name if self.name != None else self.player
+        return f"{self.name}-{self.player}" if self.name != None else self.player
 
     def __getitem__(self, index): #returns x or y for 0 or 1, respectively
         if type(index) is int and index in [0,1]: return self._position[index]
@@ -108,21 +95,18 @@ class Piece():
 
 
 class Rook(Piece):#Needs Castling
-
-    def get_moves(self, board):
+    def get_moves(self, board, b=False):
         return self.get_straight(board)
  
 class Queen(Piece):#Finished
-
-    def get_moves(self, board):
+    def get_moves(self, board, b=False):
         move_d = self.get_diagnol(board)
         move_s = self.get_straight(board)
 
         return move_d+move_s
 
 class King(Piece):#Needs Castling and Check Mating Prevention
-
-    def get_moves(self, board):
+    def get_moves(self, board, b=False):
         position = self._position
         moves = []
         for i in [-1,0,1]:
@@ -131,20 +115,17 @@ class King(Piece):#Needs Castling and Check Mating Prevention
                     moves.append((position[0]+i, position[1]+j)) if board[position[1]+j][position[0]+i]==0 or board[position[1]+j][position[0]+i].player!=self.player else None
 
         return moves
-
  
 class Bishop(Piece): #Finished
-
-    def get_moves(self, board):
+    def get_moves(self, board, b=False):
         return self.get_diagnol(board)
 
 class Pawn(Piece):#Stil needs work + en passant
-
-    def get_moves(self, board):
+    def get_moves(self, board, b=False):
         position = self._position
         moves = []
 
-        term = -1 if self.player==1 else 1
+        term = -1 if not b else 1
         moves.append((position[0],position[1]+(term*1))) if 0<=position[1]+(term*1)<=7 and 0<=position[0]<=7 and (board[position[1]+(term*1)][position[0]]==0) else None
 
         for i in [-1,1]: #takes
@@ -155,13 +136,10 @@ class Pawn(Piece):#Stil needs work + en passant
             if 0<=position[0]<=7 and 0<=position[1]+(term*2)<=7 and board[position[1]+(term*1)][position[0]]==0:
                 moves.append((position[0],position[1]+(term*2))) if board[position[1]+(term*2)][position[0]]==0 else None
 
-        # self.check_mate(board) causes recurion
         return moves
 
-
 class Knight(Piece):#Finished
-
-    def get_moves(self, board):
+    def get_moves(self, board, b=False):
         position = self._position
         moves = []
         moves.append((position[0]+1, position[1]+2)) if 0<=position[0]+1<=7 and 0<=position[1]+2<=7 and (board[position[1]+2][position[0]+1] == 0 or board[position[1]+2][position[0]+1].player != self.player) else None
